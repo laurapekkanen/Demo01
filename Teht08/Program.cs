d73a5ddd50ef21e703acb77117ea3d5b717a570c
Laura Pekkanen <k8966@student.jamk.fi>
2017-01-24 21:58:36 +0200

    more failure

--- Teht08/Program.cs
+++ Teht08/Program.cs
@@ -10,21 +10,27 @@ namespace Teht08
     {
         static void Main(string[] args)
         {
-            int[] luvut;
+            int luku1, luku2, luku3;
 
-            Console.WriteLine("Anna luku: ");
+            Console.WriteLine("Anna luku 1: ");
             string line = Console.ReadLine();
-            luvut[1] = int.Parse(line);
+            luku1 = int.Parse(line);
 
-            Console.WriteLine("Anna luku: ");
+            Console.WriteLine("Anna luku 2: ");
             line = Console.ReadLine();
-            luvut[2] = int.Parse(line);
+            luku2 = int.Parse(line);
 
-            Console.WriteLine("Anna luku: ");
+            Console.WriteLine("Anna luku 3: ");
             line = Console.ReadLine();
-            luvut[3] = int.Parse(line);
+            luku3 = int.Parse(line);
 
-            
+            if (luku1>luku2 && luku2>luku3 || luku1>luku3 && luku3>luku2)
+                Console.WriteLine("Syöttämäsi luku: " +luku1+ " on suurin");
+            if (luku2 > luku1 && luku1 > luku3 || luku2 > luku3 && luku3 > luku1)
+                Console.WriteLine("Syöttämäsi luku: " + luku2 + " on suurin");
+            if (luku3 > luku2 && luku2 > luku1 || luku3 > luku2 && luku1 > luku2)
+                Console.WriteLine("Syöttämäsi luku: " + luku3 + " on suurin");
+
         }
     }
 }
---
+++ Teht09/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Teht09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int luku;
+            int[] luvut;
+
+            do
+            {
+                Console.WriteLine("Anna luku: ");
+                string line = Console.ReadLine();
+                luku = int.Parse(line);
+                //luvut[] = int.Parse(line);
+            } while (luku != 0);
+
+            Console.WriteLine("Lukujen summa: " );*/
+
+            int[] luvut;
+            Console.WriteLine("Anna luku: ");
+            string line = Console.ReadLine();
+            luvut = int[].Parse(line);
+        }
+    }
+}
